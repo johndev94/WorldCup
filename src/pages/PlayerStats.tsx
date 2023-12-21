7440b16336015a74775aa651746d33a925c52878
John Devaney <johndevaney1@hotmail.com>
2023-12-21 04:08:32 +0000

    Player stat changed, not sure if better

--- src/pages/PlayerStats.tsx
+++ src/pages/PlayerStats.tsx
@@ -7,38 +7,45 @@ interface Player {
   team_name: string;
   statValue: number;
   points: number;
-  tackles: number; // Points or Tackles value
+  tackles: number;
 }
 
-const PlayerStats: React.FC = () => {
+const PlayerStats = () => {
   const [players, setPlayers] = useState<Player[]>([]);
-  const [statType, setStatType] = useState<string>("points"); // "points" or "tackles"
+  const [statType, setStatType] = useState("points");
 
-  useEffect(() => {
+  function fetchPlayers() {
     const endpoint = statType === "points" ? "/playerPoints" : "/playerTackles";
     axios
-      .get<Player[]>(`http://localhost:3000${endpoint}`)
-      .then((response) => {
-        const mappedData = response.data.map((item) => ({
-          player_id: item.player_id,
-          player_name: item.player_name,
-          team_name: item.team_name,
-          points: item.points,
-          tackles: item.tackles,
-          statValue: statType === "points" ? item.points : item.tackles,
-        }));
+      .get(`http://localhost:3000${endpoint}`)
+      .then(function (response) {
+        const mappedData = response.data.map(function (item: { player_id: any; player_name: any; team_name: any; points: any; tackles: any; }) {
+          return {
+            player_id: item.player_id,
+            player_name: item.player_name,
+            team_name: item.team_name,
+            points: item.points,
+            tackles: item.tackles,
+            statValue: statType === "points" ? item.points : item.tackles,
+          };
+        });
         setPlayers(mappedData);
       })
-      .catch((error) => {
+      .catch(function (error) {
         console.error(error);
       });
-  }, [statType]);
+  }
 
-  const handleStatTypeChange = (
-    event: React.ChangeEvent<HTMLSelectElement>
-  ) => {
+  function handleStatTypeChange(event: { target: { value: React.SetStateAction<string>; }; }) {
     setStatType(event.target.value);
-  };
+  }
+
+  useEffect(
+    function () {
+      fetchPlayers();
+    },
+    [statType]
+  );
 
   return (
     <div>
@@ -46,7 +53,8 @@ const PlayerStats: React.FC = () => {
       <hr />
 
       <div>
-        Stat:<br />
+        Stat:
+        <br />
         <select
           id="statTypeSelect"
           value={statType}
@@ -69,14 +77,16 @@ const PlayerStats: React.FC = () => {
           </tr>
         </thead>
         <tbody>
-          {players.map((player, index) => (
-            <tr key={player.player_id}>
-              <td>{index + 1}</td>
-              <td>{player.player_name}</td>
-              <td>{player.team_name}</td>
-              <td>{player.statValue}</td>
-            </tr>
-          ))}
+          {players.map(function (player, index) {
+            return (
+              <tr key={player.player_id}>
+                <td>{index + 1}</td>
+                <td>{player.player_name}</td>
+                <td>{player.team_name}</td>
+                <td>{player.statValue}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
